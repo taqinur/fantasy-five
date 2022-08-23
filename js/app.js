2f36f7a7c2155647bc07890c74980479a09579e4
Taqinur <taqi1662@gmail.com>
2022-08-23 23:23:20 +0600

    Completed task

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ function showPlayerList(playerList){
     orderedPlayerList.innerHTML = "";
 
     for (let i =0; i<playerList.length; i++){
+        // Maximum 5 players can be added
         if(i<5){
             const selectedPlayerName = playerArray[i];
 
@@ -17,37 +18,52 @@ function showPlayerList(playerList){
             `;
 
             orderedPlayerList.appendChild(ol);
-        }
-        else{
-            alert('Five players is already selected');
-        }
 
+        // player Expense calculation
         function playerExpenseTotal(){
-            const costPerPlayers = parseFloat(document.getElementById('costPerPlayer').value);
-            console.log(costPerPlayers);
+            const costPerPlayerString = document.getElementById('costPerPlayer').value;
+            const costPerPlayers = parseFloat(costPerPlayerString);
             const numberOfPlayers = i+1; 
-            const playerExpense = costPerPlayers * numberOfPlayers; //need change
-            document.getElementById('player-expenses').innerText = playerExpense;
-            return playerExpense;
+            const playerExpense = costPerPlayers * numberOfPlayers;
+            if(isNaN(playerExpense)){
+                alert('Please input a value in the box');
+            }
+            else{
+                document.getElementById('player-expenses').innerText = playerExpense;
+                return playerExpense;
+            }
         }
 
         document.getElementById('calculate-players-cost').addEventListener('click', function(){
             playerExpenseTotal();
         })
 
+        // Total Expense Calculation
         document.getElementById('calculate-total').addEventListener('click', function(){
             const playercost = playerExpenseTotal();
             const manager = document.getElementById('cost-Manager').value;
             const coach = document.getElementById('cost-Coach').value;
             const totalCost = parseFloat(playercost) + parseFloat(manager) + parseFloat(coach);
-            document.getElementById('total-expenses').innerText = totalCost;
+            if(isNaN(totalCost)){
+                alert('Please input a value in each box');
+            }
+            else{
+                document.getElementById('total-expenses').innerText = totalCost;
+            }
         })
+        }
+
+        // Alert for click when 5 players are already added
+        else{
+            alert('Five players is already selected');
+        }
     }
 }
 
 
 function selectPlayer(element){
     const playerName = element.parentNode.children[0].innerText;
+    element.disabled = true;
     playerArray.push(playerName);
     // console.log(playerArray);
     showPlayerList(playerArray);
